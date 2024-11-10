a05bfe5f00e4ac9f655f2cf9ede19a6b4fcfd652
Hooria Sheikh <hooriashaikh743@gmail.com>
2024-11-10 18:37:36 +0500

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" bg-black  text-white mt-16 py-6 text-center bg-blue">
+      © Hooria | All rights reserved 2024.
+    </div>
+  );
+};
+
+export default Footer;
